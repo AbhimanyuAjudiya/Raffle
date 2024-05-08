63a80ef9c0897e91c52afdd4e3e2e649271e91d7
Abhimanyu Ajudiya <abhimanyuajudiya@gmail.com>
2024-05-09 00:53:45 +0530

    converted from JS to TS

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -18,7 +18,7 @@ error Raffle__RaffleNotOpen();
 
 /**
  * @title A sample Raffle Contract
- * @author Abhimanyu Ajudiya
+ * @author Abhimanyu Ajudiya 
  * @notice This contract is for creating a sample raffle contract
  * @dev This implements the Chainlink VRF Version 2
  */
